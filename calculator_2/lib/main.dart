c4396faa7819df65447e20cce7378ea1f3797607
Rodrigo Inoue <pt234989@IFSPROOT.PTB>
2022-04-27 10:59:59 -0300

    Learn about styling and fonts, added new buttons

--- calculator_2/lib/main.dart
+++ calculator_2/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -39,26 +42,93 @@ class _CalculadoraState extends State<Calculadora>{
         Padding(
           padding: const EdgeInsets.all(20),
           child: TextField(
+            decoration: InputDecoration(
+              helperText: "Informe o primeiro valor",
+              filled: true,
+              fillColor: Colors.blue.shade50,
+              prefixIcon: Icon(Icons.numbers),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+                borderSide: BorderSide.none
+              ),
+            ),
             controller: _controller1,
             keyboardType: TextInputType.number,
           ),
         ),
-        ElevatedButton(
-          onPressed: (){
-            setState((){
-              result = int.parse(_controller1.text) + int.parse(_controller2.text);
-            });
-          }, 
-          child: Text("+")
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: ElevatedButton(
+                onPressed: (){
+                  setState((){
+                    result = int.parse(_controller1.text) + int.parse(_controller2.text);
+                  });
+                }, 
+                child: Text("+")
+              )
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: ElevatedButton(
+                onPressed: (){
+                  setState((){
+                    result = int.parse(_controller1.text) - int.parse(_controller2.text);
+                  });
+                }, 
+                child: Text("-")
+              )
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: ElevatedButton(
+                onPressed: (){
+                  setState((){
+                    result = int.parse(_controller1.text) * int.parse(_controller2.text);
+                  });
+                }, 
+                child: Text("*")
+              )
+            ),
+          ],
         ),
         Padding(
           padding: const EdgeInsets.all(20),
           child: TextField(
+            decoration: InputDecoration(
+              helperText: "Informe o segundo valor",
+              filled: true,
+              fillColor: Colors.blue.shade50,
+              prefixIcon: Icon(Icons.numbers),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+                borderSide: BorderSide.none
+              ),
+            ),
             controller: _controller2,
             keyboardType: TextInputType.number,
           ),
         ),
-        Text("Resultado: ${result ?? ''}")
+        Text.rich(
+          TextSpan(
+            text: "Resultado: ",
+            style: GoogleFonts.aladin(
+              fontSize: 26
+            ),
+            children: [
+              TextSpan(
+                text: "${result ?? ''}",
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                )
+              )
+            ]
+
+          )
+        ),
         
       ],
     );
