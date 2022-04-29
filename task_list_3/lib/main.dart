10725f118a0659b9b1f04531ae9f2254c0a1dd8e
Rodrigo Inoue <rodrigoinoue1122@gmail.com>
2022-04-28 23:27:48 -0300

    Added FAB and his dialog

--- task_list_3/lib/main.dart
+++ task_list_3/lib/main.dart
@@ -13,26 +13,60 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  TextEditingController _task = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Tarefas do dia'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: (){
+          showDialog(
+            context: context, 
+            builder: (BuildContext build){
+              return AlertDialog(
+                title: Text("Adicionar Tarefa"),
+                content: Text("Conteúdo do Dialog"),
+                actions: [
+                  TextField(
+                    controller: _task,
+                  ),
+                  TextButton(
+                    onPressed: (){
+                      Navigator.of(context).pop();
+                    }, 
+                    child: Text("Cancelar")
+                  ),
+                  TextButton(
+                    onPressed: (){
+                      print(_task.text);
+                      Navigator.of(context).pop();
+                    },
+                    child: Text("OK")
+                  ),
+                ],
+
+              );
+            }
+          );
+        },
       ),
     );
   }
