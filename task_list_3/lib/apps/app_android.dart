ab6f947f18582c676fb9e1fa1dedff30ffdd5e1c
Rodrigo Inoue <pt234989@IFSPROOT.PTB>
2022-05-02 15:22:43 -0300

    Refactoring pages and adding a ios layout

---
+++ task_list_3/lib/apps/app_android.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import '../pages/home_page_android.dart';
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: const MyHomePage(),
+    );
+  }
+}
---
+++ task_list_3/lib/pages/home_page_android.dart
@@ -0,0 +1,111 @@
+
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController _task = TextEditingController();
+
+  List<Widget> listItems = [];
+
+  bool isDone = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Tarefas do dia')),
+      ),
+      body: ListView.separated(
+        itemCount: listItems.length,
+        itemBuilder: (BuildContext context, int position) {
+          return listItems[position];
+        },
+        separatorBuilder: (BuildContext context, int position) => Divider(
+          color: Colors.grey.shade600,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext build) {
+              return AlertDialog(
+                title: Text("Adicionar Tarefa"),
+                // content: Text("Informe a nova tarefa"),
+                actions: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                    child: TextField(
+                      controller: _task,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                        style: TextButton.styleFrom(
+                          textStyle: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.red.shade100,
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text("Cancelar"),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          setState(() {
+                            listItems.add(ListItens(title: _task.text));
+                            _task.text = "";
+                          });
+                          Navigator.of(context).pop();
+                        },
+                        child: Text("OK"),
+                      ),
+                    ],
+                  ),
+                ],
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class ListItens extends StatefulWidget {
+  ListItens({Key? key, required this.title, this.isDone = false})
+      : super(key: key);
+
+  String title;
+  bool isDone;
+
+  @override
+  State<ListItens> createState() => _ListItens();
+}
+
+class _ListItens extends State<ListItens> {
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(widget.title),
+      trailing: widget.isDone ? Icon(Icons.done) : null,
+      // leading: widget.isDone ? Icon(Icons.done) : null,
+      onTap: () {
+        setState(() {
+          widget.isDone = !widget.isDone;
+        });
+      },
+    );
+  }
+}
