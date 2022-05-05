b16293a6ec1be17cfd7373eb083fd63026aafd4b
Innoue <rodrigoinoue1122@gmail.com>
2022-05-05 17:40:27 -0300

    Future builder

--- movie_list_4/lib/screens/home_page.dart
+++ movie_list_4/lib/screens/home_page.dart
@@ -1,20 +1,42 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:movie_list_4/domain/models/movie.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
-    var url = Uri.parse('https://api.themoviedb.org/3/movie/popular?api_key=9c3994290de16f7140dbc27e62cf3163&page=1');
-    http.post(url).then((value){
-      print('StatusCode: ${value.statusCode}\nResult: ${value.body}');  
-    });
-
     return Scaffold(
       appBar: AppBar(title: Text('Lista de Filmes')),
-      body: Container(),
+      body: FutureBuilder<List<Movie>>(
+        future: _getMovies(),
+        builder: (context, snapshot){
+          if(snapshot.hasData){
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index){
+                final movie = snapshot.data![index];
+                return ListTile(
+                  title: Text(movie.title),
+                  subtitle: Text(movie.overview),
+                );
+              },
+            );
+          }else{
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
     );
+  }
+
+  Future<List<Movie>> _getMovies() async {
+    var url = Uri.parse('https://api.themoviedb.org/3/movie/popular?api_key=9c3994290de16f7140dbc27e62cf3163&page=1');
+    final response = await http.post(url);
+    final json = jsonDecode(response.body);
+    return MovieData.fromJson(json).movies;
   }
 }
