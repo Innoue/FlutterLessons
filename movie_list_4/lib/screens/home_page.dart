814f8fbca67f42357048abbeca1de55814451a51
Innoue <rodrigoinoue1122@gmail.com>
2022-05-05 23:14:27 -0300

    Added images

--- movie_list_4/lib/screens/home_page.dart
+++ movie_list_4/lib/screens/home_page.dart
@@ -1,42 +1,50 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:movie_list_4/domain/models/movie.dart';
+import 'package:movie_list_4/services/movie_services.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key);
+  MovieService movieService = MovieService();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Lista de Filmes')),
       body: FutureBuilder<List<Movie>>(
-        future: _getMovies(),
-        builder: (context, snapshot){
-          if(snapshot.hasData){
+        future: movieService.getMovies(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
             return ListView.builder(
               itemCount: snapshot.data!.length,
-              itemBuilder: (context, index){
+              itemBuilder: (context, index) {
                 final movie = snapshot.data![index];
                 return ListTile(
-                  title: Text(movie.title),
-                  subtitle: Text(movie.overview),
+                  title: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Text(
+                      movie.title,
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  subtitle: Text(
+                    movie.overview,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  leading: Image.network(
+                    'https://image.tmdb.org/t/p/w500${movie.backdropPath}',
+                  ),
                 );
               },
             );
-          }else{
+          } else {
             return Center(child: CircularProgressIndicator());
           }
         },
       ),
     );
-  }
-
-  Future<List<Movie>> _getMovies() async {
-    var url = Uri.parse('https://api.themoviedb.org/3/movie/popular?api_key=9c3994290de16f7140dbc27e62cf3163&page=1');
-    final response = await http.post(url);
-    final json = jsonDecode(response.body);
-    return MovieData.fromJson(json).movies;
   }
 }
